143407426bb437f7aa4b31a6cac7a96d9657188f
BoitumeloMeletse <2memusicbookings@gmail.com>
2025-07-11 01:55:00 +0200

    we

--- controllers/users.js
+++ controllers/users.js
@@ -3,11 +3,10 @@ const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
     try {
-        // Note: Changed getdatabase() to getDatabase() - case sensitive!
         const database = mongodb.getDatabase();
-        const result = database.db().collection('users').find();
+        const result = database.db('project1').collection('users').find();
         const users = await result.toArray();
-        
+
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(users);
     } catch (error) {
@@ -21,16 +20,16 @@ const getSingle = async (req, res) => {
         if (!ObjectId.isValid(req.params.id)) {
             return res.status(400).json({ error: 'Invalid user ID format' });
         }
-        
+
         const userId = new ObjectId(req.params.id);
         const database = mongodb.getDatabase();
-        const result = database.db().collection('users').find({ _id: userId });
+        const result = database.db('project1').collection('users').find({ _id: userId });
         const users = await result.toArray();
-        
+
         if (users.length === 0) {
             return res.status(404).json({ message: 'User not found' });
         }
-        
+
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(users[0]);
     } catch (error) {
