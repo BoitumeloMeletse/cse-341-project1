ff5f0d2f02b66885790938b3aac5d1f5844054b0
BoitumeloMeletse <2memusicbookings@gmail.com>
2025-08-02 04:55:48 +0200

    wewe

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
     description: 'API for managing contact and task data including CRUD operations.',
   },
   host: 'cse-341-project1-0cop.onrender.com', // Change to Render domain before upload
-  schemes: ['http', 'https'],
+  schemes: ['https'],
 };
 
 const outputFile = './swagger_output.json';
