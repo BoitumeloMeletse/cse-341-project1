5f337f2efa3da5cbe3b280648146a65d77eeecf8
BoitumeloMeletse <2memusicbookings@gmail.com>
2025-08-16 02:46:48 +0200

    ytf

--- swagger.js
+++ swagger.js
@@ -3,7 +3,7 @@ const swaggerAutogen = require('swagger-autogen')();
 const doc = {
   info: {
     title: 'Contacts, Tasks, Menu, Orders, Users & Payments API',
-    description: 'API for managing contacts, tasks, food menu, and orders with CRUD operations.',
+    description: 'API for managing contacts, tasks, food menu, orders, users, and payments with CRUD operations.',
   },
   host: 'cse-341-project1-0cop.onrender.com', // Change to Render domain before upload
   schemes: ['https'],
